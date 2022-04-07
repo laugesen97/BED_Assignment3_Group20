1df7b274df8c460ef2d8130066c03e63b42c43bd
Frederik Damgaard Laugesen <au651033@uni.au.dk>
2022-04-07 11:21:22 +0200

    Added breakfast checkin page

---
+++ Assignment3_Group20/Assignment3_Group20/Pages/BreakfastCheckIn.cshtml.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Assignment3_Group20.Data;
+using Assignment3_Group20.Model;
+
+namespace Assignment3_Group20.Pages
+{
+    public class BreakfastCheckInModel : PageModel
+    {
+        private readonly Assignment3_Group20.Data.ApplicationDbContext _context;
+
+        public BreakfastCheckInModel(Assignment3_Group20.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+            Reservation.isCheckedIn = DateTime.Now;
+            _context.Reservation.Add(Reservation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
