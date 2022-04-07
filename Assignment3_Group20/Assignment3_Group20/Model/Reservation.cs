2b4fb76a62918c1c5b23f3d387ae75914035fdb5
Frederik Damgaard Laugesen <au651033@uni.au.dk>
2022-04-07 10:30:14 +0200

    Updated model

---
+++ Assignment3_Group20/Assignment3_Group20/Model/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment3_Group20.Model
+{
+    public class Reservation
+    {
+        public int Adults { get; set; }
+        public int Children { get; set; }
+        public bool isCheckedIn { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
