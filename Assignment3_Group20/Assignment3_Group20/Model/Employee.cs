9b929d1de350e689133c27baef459d5f42dc79f6
Frederik Damgaard Laugesen <au651033@uni.au.dk>
2022-04-07 10:25:32 +0200

    Few models added

---
+++ Assignment3_Group20/Assignment3_Group20/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment3_Group20.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+    public class Receptionist : Employee
+    {
+
+    }
+    public class KitchenEmployee : Employee
+    {
+
+    }
+}
---
+++ Assignment3_Group20/Assignment3_Group20/Model/Guest.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment3_Group20.Model
+{
+    public class Guest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int RoomNumber { get; set; }
+    }
+}
